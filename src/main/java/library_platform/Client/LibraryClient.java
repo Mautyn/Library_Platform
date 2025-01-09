d53a6062ed9e6d8d259b87223fb8875d4b1d0f6e
Mautyn <piotr.martyn@wp.pl>
2025-01-09 19:30:27 +0100

    new

--- src/main/java/library_platform/Client/LibraryClient.java
+++ src/main/java/library_platform/Client/LibraryClient.java
@@ -2,23 +2,14 @@ package library_platform.Client;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import library_platform.Shared.Book;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.*;
 import java.io.*;
-import java.net.*;
-import java.util.List;
 
 public class LibraryClient extends Application {
-    private ObjectOutputStream out;
-    private ObjectInputStream in;
-
-    private ListView<String> booksListView = new ListView<>();
-    private Button borrowButton = new Button("Borrow");
 
     @Override
     public void start(Stage primaryStage) throws Exception {

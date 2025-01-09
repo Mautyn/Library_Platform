93f58ad827bd227e810e15099c21f0311781a769
Mautyn <piotr.martyn@wp.pl>
2025-01-09 20:48:54 +0100

    new

--- src/main/java/library_platform/Client/view/LoginController.java
+++ src/main/java/library_platform/Client/view/LoginController.java
@@ -1,4 +1,43 @@
 package library_platform.Client.view;
 
+import library_platform.Client.SceneController;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import javafx.event.ActionEvent;
+
+import java.awt.*;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class LoginController {
+    @FXML
+    private JFXButton Log_InButton;
+
+    @FXML
+    private JFXButton ToRegisterButton;
+
+    @FXML
+    private TextField LoginTextField;
+
+    @FXML
+    private TextField PasswordTextField;
+
+    public void onLog_inClick(ActionEvent event) {
+        try {
+            SceneController.setScene(event, "/library_platform/loginScene.fxml");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void onRegisterClick(ActionEvent event) {
+        try {
+            SceneController.setScene(event, "/library_platform/loginScene.fxml");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
---
+++ src/main/java/library_platform/Client/view/RegisterController.java
@@ -0,0 +1,26 @@
+package library_platform.Client.view;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class RegisterController {
+    @FXML
+    private Checkbox TermsCheckBox;
+
+    @FXML
+    private JFXButton Register_Button;
+
+    @FXML
+    private TextField RegisterLoginTextField;
+
+    @FXML
+    private TextField ConfirmLoginTextField;
+
+    @FXML
+    private TextField RegisterPasswordTextField;
+
+    @FXML
+    private TextField ConfirmPasswordTextField;
+}
